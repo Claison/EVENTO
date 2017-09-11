834e0a949b0d3c442e7d645667240b5fe2172017
Claison <30608657+Claison@users.noreply.github.com>
2017-09-11 19:16:08 -0300

    remoçao classe

--- evento_01/evento_trabalho1/admin.py
+++ evento_01/evento_trabalho1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from evento_trabalho1.models import PessoaFisica, PessoaJuridica, Autor, Evento, EventoCientifico, ArtigoCientifico, Publicacao
+from evento_trabalho1.models import PessoaFisica, PessoaJuridica, Autor, Evento, EventoCientifico, ArtigoCientifico
 
 admin.site.register(PessoaFisica)
 admin.site.register(PessoaJuridica)
@@ -7,5 +7,4 @@ admin.site.register(Autor)
 admin.site.register(Evento)
 admin.site.register(EventoCientifico)
 admin.site.register(ArtigoCientifico)
-admin.site.register(Publicacao)
 # Register your models here.
